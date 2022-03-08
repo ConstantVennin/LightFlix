c6f9f1534a1865cf8c51b987412a01e208c47895
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2022-03-08 11:29:05 +0100

    Liste de tous les films

--- src/components/SerieThumbnail.js
+++ src/components/SerieThumbnail.js
@@ -2,7 +2,8 @@ import Component from './Component.js';
 import Img from './Img.js';
 
 export default class SerieThumbnail extends Component {
-	constructor({ name, image, description, episodes }) {
+	constructor(name, image, description, episodes) {
+		console.log('rfhzf', name);
 		super('article', { name: 'class', value: 'serieThumbnail' }, [
 			new Component('a', { name: 'href', value: image }, [
 				new Component('h1', null, name),
--- src/pages/PizzaList.js
+++ src/pages/PizzaList.js
@@ -3,29 +3,29 @@ import Page from './Page';
 import $ from 'jquery';
 
 export default class PizzaList extends Page {
-	// #pizzas;
-	// constructor(pizzas) {
-	// 	super('pizzaList'); // on pase juste la classe CSS souhaitée
-	// 	this.pizzas = pizzas;
-	// }
-	// mount(element) {
-	// 	// super.mount(element);
-	// 	// // appel ajax lorsque l'on affiche la page
-	// 	// $(this.element).addClass('is-loading');
-	// 	// fetch('http://localhost:8080/api/v1/pizzas')
-	// 	// 	.then(response => response.json())
-	// 	// 	.then(data => (this.pizzas = data)) //  maj des children
-	// 	// 	.then(() => this.element.html(this.render())) // affichage dans la page
-	// 	// 	.then(() => {
-	// 	// 		$('.pizzaList .pizzaThumbnail a', this.element).on('click', event => {
-	// 	// 			event.preventDefault();
-	// 	// 			console.log(event.currentTarget.getAttribute('href'));
-	// 	// 		});
-	// 	// 	})
-	// 	// 	.then($(this.element).removeClass('is-loading'));
-	// }
-	// set pizzas(value) {
-	// 	// this.#pizzas = value;
-	// 	// this.children = this.#pizzas.map(pizza => new PizzaThumbnail(pizza));
-	// }
+	#pizzas;
+	constructor(pizzas) {
+		super('pizzaList'); // on pase juste la classe CSS souhaitée
+		this.pizzas = pizzas;
+	}
+	mount(element) {
+		super.mount(element);
+		// appel ajax lorsque l'on affiche la page
+		$(this.element).addClass('is-loading');
+		fetch('http://localhost:8080/api/v1/pizzas')
+			.then(response => response.json())
+			.then(data => (this.pizzas = data)) //  maj des children
+			.then(() => this.element.html(this.render())) // affichage dans la page
+			.then(() => {
+				$('.pizzaList .pizzaThumbnail a', this.element).on('click', event => {
+					event.preventDefault();
+					console.log(event.currentTarget.getAttribute('href'));
+				});
+			})
+			.then($(this.element).removeClass('is-loading'));
+	}
+	set pizzas(value) {
+		this.#pizzas = value;
+		this.children = this.#pizzas.map(pizza => new PizzaThumbnail(pizza));
+	}
 }
