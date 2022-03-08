0629484bb440d07211bcab83138ef69efa2f2277
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2022-03-08 10:04:35 +0100

    clean du js existant + component film

---
+++ src/components/SerieThumbnail.js
@@ -0,0 +1,15 @@
+import Component from './Component.js';
+import Img from './Img.js';
+
+export default class SerieThumbnail extends Component {
+	constructor({ name, image, description, episodes }) {
+		super('article', { name: 'class', value: 'serieThumbnail' }, [
+			new Component('a', { name: 'href', value: image }, [
+				new Component('h1', null, name),
+				new Img(image),
+				new Component('h4', null, description),
+				new Component('p', null, `Episodes : ${episodes}`),
+			]),
+		]);
+	}
+}
